6f1dac7660102f0392064c5337382a3b3d23feb9
koylyak andriy <koylyakandriy@gmail.com>
2021-07-05 08:58:47 +0300

    M-01-useFetch - create hooks - add loader - create detail page

---
+++ src/modules/DetailsPage/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch'
+import Detail from '../../components/Detail'
+import Loader from '../../components/Loader'
+
+const DetailsPage = () => {
+  const { id } = useParams()
+  const apiUrl = `comics/${id}`
+  const [{ response, isLoading }, doFetch] = useFetch(apiUrl)
+
+  useEffect(() => {
+    doFetch()
+  }, [doFetch])
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {!isLoading && response && <Detail data={response} />}
+    </>
+  )
+}
+
+export default DetailsPage
