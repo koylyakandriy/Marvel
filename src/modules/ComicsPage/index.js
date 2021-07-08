81a675ab528deffc2ced4383af87c02c0c63a533
koylyak andriy <koylyakandriy@gmail.com>
2021-07-08 07:06:50 +0300

    M-01-

--- src/modules/ComicsPage/index.js
+++ src/modules/ComicsPage/index.js
@@ -1,22 +1,64 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Loader from '../../components/Loader'
 import CardItem from '../../components/Card'
 import useFetch from '../../hooks/useFetch'
+import { Row } from 'react-bootstrap'
 
 const MoviesPage = () => {
-  const apiUrl = 'comics'
+  const baseApiUrl = 'comics'
+  const moreItems = 20
+  const maxLimit = 100
+  const [apiUrl, setApiUrl] = useState(baseApiUrl)
+  const [limit, setLimit] = useState(0)
   const [{ response, isLoading }, doFetch] = useFetch(apiUrl)
 
   useEffect(() => {
     doFetch()
-  }, [doFetch])
+  }, [doFetch, apiUrl])
+
+  useEffect(() => {
+    if (response) {
+      setLimit(response?.limit)
+    }
+  }, [response])
+
+  const onLoad = () => {
+    setApiUrl(`${baseApiUrl}?limit=${limit + moreItems}`)
+  }
+
+  const pageEnd = useRef()
+  useEffect(() => {
+    if (!isLoading && response) {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            onLoad()
+            if (limit >= maxLimit) {
+              observer.unobserve(pageEnd.current)
+            }
+          }
+        },
+        { threshold: 1 }
+      )
+      observer.observe(pageEnd.current)
+    }
+  }, [onLoad, isLoading])
 
   return (
     <>
-      {isLoading && <Loader />}
-      {!isLoading &&
-        response &&
-        response.results.map((item) => <CardItem key={item.id} item={item} />)}
+      <Row style={{ alignItems: 'flex-start' }}>
+        {isLoading && <Loader />}
+        {!isLoading &&
+          response &&
+          response.results.map((item) => (
+            <CardItem key={item.id} item={item} />
+          ))}
+      </Row>
+      <Row fluid className="justify-content-center">
+        <button onClick={onLoad} ref={pageEnd}>
+          More
+        </button>
+      </Row>
     </>
   )
 }
